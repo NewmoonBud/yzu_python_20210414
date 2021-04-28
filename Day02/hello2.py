b0349a57e2857707f6fa9f0b4d99afe1c39261bc
NewMoonBud <NewMoonBud@gmail.com>
2021-04-28 20:20:46 +0800

    Python

---
+++ Day02/hello2.py
@@ -0,0 +1,19 @@
+# -*- coding:UTF-8 -*-
+chinese = "100"  # str
+english = "90"   # str
+math = 80        # int
+print(chinese, english, math)
+
+sum = chinese + english
+print(sum)
+
+sum = chinese + english + math
+print(sum)
+
+sum = int(chinese) + int(english) + math
+print(sum)
+
+print(type(chinese), type(english), type(math)
+
+print("總分:" + str(sum))
+
