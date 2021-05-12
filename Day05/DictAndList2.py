3e7c59eb78bc61a920a6685c5f764ecce3933dc2
NewMoonBud <NewMoonBud@gmail.com>
2021-05-12 21:31:31 +0800

    Python

--- Day05/DictAndList2.py
+++ Day05/DictAndList2.py
@@ -4,7 +4,12 @@ e3 = {'name': 'Bob', 'salary': 70000, 'program': ['Java', 'C#', 'python']}
 
 # 請計算會 python 的員工人名，並將放入到 name[]中
 emps = [e1, e2, e3]
-name = []
-for key in emps:
-    if emps[program] = python
-        name =
+target = 'python'
+names = []
+for emp in emps:
+    # print(emp['name'], emp['program'])
+    for p in emp['program']:
+        if p == target:
+            # print(emp['name'],p)
+            names.append(emp['name'])
+print(names)
